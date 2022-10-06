9649b9d433d63bd5a9a4d3c15f2934a5266ad639
0tak2 <0tak2.code@gmail.com>
2022-10-06 09:58:37 +0900

    데이터 수정 기능 구횬

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # TODO: 엑셀 익스포트
 
 import os
+import subprocess
 import sillookParser
 import urllib3
 from datetime import datetime
@@ -43,8 +44,7 @@ def enterMenu(cmd: str) -> None:
     elif cmd == 'A' or cmd == 'a' or cmd == 'ㅁ' or cmd == 'Add':
         addDataPrompt()
     elif cmd == 'M' or cmd == 'm' or cmd == 'ㅡ' or cmd == 'Modify':
-        console.print('수정')
-        input()
+        modifyPrompt()
     elif cmd == 'E' or cmd == 'e' or cmd == 'ㄷ' or cmd == 'Export':
         exportPrompt()
     elif cmd == 'B' or cmd == 'b' or cmd == 'ㅠ' or cmd == 'Basket':
@@ -61,6 +61,124 @@ def enterMenu(cmd: str) -> None:
         console.print("!!! 잘못된 명령을 입력했습니다.\n아래와 같은 명령을 실행할 수 있습니다.\n", style="bold")
         printHelp()
         console.print("\n메인 메뉴로 돌아가려면 엔터를 입력하세요.", style="bold")
+        input()
+
+def modifyPrompt() -> None:
+    clear()
+    console.print("\n\n====== 데이터 수정 / 삭제 ======\n", style="bold yellow")
+
+    index = None
+
+    if globals.getViewMode() == 0:
+        count = showAllDataPanel()
+        if count < 0:
+            console.print("!!! 수정할 데이터가 없습니다.", style="bold yellow")
+
+        console.print(f"> 수정할 데이터의 DB ID 입력 <기본값: 1>: ", style="bold yellow")
+        index = input()
+        if index == '':
+            index = 1
+            
+    elif globals.getViewMode() == 1:
+        index = globals.getCurrentId()
+
+    showDetailDataPanel(index)
+    console.print("\n! 아래에 수정할 컬럼을 입력해주세요. 데이터 전체를 삭제하려면 X를 입력해주세요.\n메모=M, 제목=T, 위치=L, 국문내용=K, 원문내용=H, 삭제=X", style="bold")
+
+    while True:
+        console.print(f"> 수정할 항목을 선택: ", style="bold yellow")
+        col = input()
+        if col == 'M' or col == 'm' or col == 'ㅡ':
+            modify(index, 'note')
+            break
+        elif col == 'T' or col == 't' or col == 'ㅅ':
+            modify(index, 'title')
+            break
+        elif col == 'L' or col == 'l' or col == 'ㅣ':
+            modify(index, 'location')
+            break
+        elif col == 'K' or col == 'k' or col == 'ㅏ':
+            modify(index, 'content_kor')
+            break
+        elif col == 'H' or col == 'h' or col == 'ㅗ':
+            modify(index, 'content_han')
+            break
+        elif col == 'X' or col == 'x' or col == 'ㅌ':
+            console.print(f"> 정말로 삭제하려면 '삭제'라고 입력하세요. 이 작업은 돌이킬 수 없습니다.", style="bold red")
+            confirmInput = input()
+            if confirmInput == "삭제" or confirmInput == "'삭제'":
+                delete(index)
+                break
+        else:
+            console.print("!!! 잘못된 명령을 입력했습니다.\n아래와 같이 입력해주세요.", style="bold")
+            console.print("메모=M, 제목=T, 위치=L, 국문내용=K, 원문내용=H", style="bold")
+
+def delete(index) -> None: 
+    try:
+        conn, cur = globals.getDB()
+        db.deleteById(conn, cur, index)   
+    except Exception as e:
+        console.print("!!! 데이터 삭제 중 오류가 발생했습니다.", e, style="bold red")
+        console.print("\n메인 메뉴로 돌아가려면 엔터를 입력하세요.", style="bold red")
+        input()
+
+    console.print("* 데이터 삭제가 완료되었습니다.", style="bold blue")
+    console.print("\n메인 메뉴로 돌아가려면 엔터를 입력하세요.", style="bold blue")
+    input()
+
+def modify(index, col) -> None:
+    conn, cur = globals.getDB()
+
+    originalEntity = db.getOneById(cur, index) # 원본 데이터를 불러옴
+    originalContent = ""
+    if col == "title":
+        originalContent = originalEntity.getTitle()
+    elif col == "location":
+        originalContent = originalEntity.getLocation()
+    elif col == "content_kor":
+        originalContent = originalEntity.getContentKor()
+    elif col == "content_han":
+        originalContent = originalEntity.getContentHan()
+    elif col == "note":
+        originalContent = originalEntity.getNote()
+
+    tmpFileName = "tmp.txt" # 원본 데이터가 들어있는 텍스트 파일을 작성
+    with open(tmpFileName, 'w') as f:
+        f.write("# 데이터를 수정하고 저장한 후, 에디터를 닫으면 변경 내용이 반영됩니다.\n")
+        f.write("# '#'으로 시작하는 줄은 변경 내용에 반영되지 않습니다.\n")
+        f.write("# \n")
+        f.write(originalContent)
+
+    if IS_WINDOWS: # 에디터를 call
+        subprocess.call(f"notepad.exe {tmpFileName}")
+    else:
+        subprocess.call(f"nano {tmpFileName}")
+    
+    # 작성된 텍스트 파일의 내용을 불러옴
+    newContent = ""
+    with open(tmpFileName, 'r') as f:
+        lines = f.readlines()
+    for line in lines:
+        if line.strip()[0] == '#':
+            continue
+        else:
+            newContent = newContent + line
+    
+    try:
+        result = db.updateValue(conn, cur, index, col, newContent) # 에디터가 종료되면 UPDATE 문을 실행
+    except Exception as e:
+        console.print(e, style="bold red")
+        result = -1
+    finally:
+        os.remove(tmpFileName)
+    
+    if result == 0:
+        console.print("* 데이터 수정이 완료되었습니다.", style="bold blue")
+        console.print("\n메인 메뉴로 돌아가려면 엔터를 입력하세요.", style="bold blue")
+        input()
+    else:
+        console.print("!!! 데이터 수정 중 오류가 발생했습니다.", style="bold red")
+        console.print("\n메인 메뉴로 돌아가려면 엔터를 입력하세요.", style="bold red")
         input()
 
 def exportPrompt() -> None:
@@ -180,7 +298,7 @@ def showAllDataPanel() -> int:
     console.print(table)
     return globals.getSizeOfData()
 
-def showDetailDataPanel(id, direction=1) -> None:
+def showDetailDataPanel(id) -> None:
     currentArticle = None
 
     conn, cur = globals.getDB()
--- sillookDatabase.py
+++ sillookDatabase.py
@@ -25,18 +25,47 @@ def addArticle(conn, cursor, article: SillookArticleEntity):
     newArticleTuple = (articleId, title, location, content_kor, content_han,
         metadata, note)
     
-    cursor.execute("INSERT INTO articles (articleId, title, location, content_kor, \
-        content_han, metadata, note) values (?, ?, ?, ?, ?, ?, ?)", newArticleTuple)
-    
-    conn.commit()
+    try:
+        cursor.execute("INSERT INTO articles (articleId, title, location, content_kor, \
+            content_han, metadata, note) values (?, ?, ?, ?, ?, ?, ?)", newArticleTuple)
+        conn.commit()
+    except Exception as e:
+        raise Exception("INSERT 중 오류 발생 ", e)
 
     return getAll(cursor)
 
-def getAll(cursor):
-    data = []
+def getOneById(cursor, id):
+    try:
+        cursor.execute("SELECT * FROM articles WHERE id=?", (str(id)))
+        rows = cursor.fetchall()
+    except Exception as e:
+        raise Exception("SELECT 중 오류 발생 ", e)
 
-    cursor.execute("SELECT * FROM articles")
-    rows = cursor.fetchall()
+    if len(rows) > 1:
+        raise Exception("SELECT 중 오류 발생: 특정 id로 조회한 값의 개수가 2개 이상")
+
+    result_row = rows[0]
+
+    key = result_row[0]
+    articleId = result_row[1]
+    title = result_row[2]
+    location = result_row[3]
+    content_kor = result_row[4]
+    content_han = result_row[5]
+    metadata = result_row[6]
+    note = result_row[7]
+    
+    return SillookArticleEntity(key, articleId, title, location,
+            content_kor, content_han, metadata, note)
+
+def getAll(cursor):
+    items = []
+
+    try:
+        cursor.execute("SELECT * FROM articles")
+        rows = cursor.fetchall()
+    except Exception as e:
+        raise Exception("SELECT 중 오류 발생 ", e)
 
     for row in rows:
         key = row[0]
@@ -47,7 +76,33 @@ def getAll(cursor):
         content_han = row[5]
         metadata = row[6]
         note = row[7]
-        data.append(SillookArticleEntity(key, articleId, title, location,
+        items.append(SillookArticleEntity(key, articleId, title, location,
             content_kor, content_han, metadata, note))
     
-    return data
+    return items
+
+def updateValue(conn, cursor, id, column, newValue) -> int:
+    verified = False
+    for col in ["title", "location", "content_kor", "content_han", "note"]:
+        if column == col:
+            verified = True
+            break
+    
+    if not verified:
+        return -1
+    
+    try:
+        sql = f'UPDATE articles SET {column} = "{newValue}" WHERE id={str(id)}'
+        cursor.execute(sql)
+        conn.commit()
+    except Exception as e:
+        raise Exception("UPDATE 중 오류 발생 ", e)
+
+    return 0
+
+def deleteById(conn, cursor, id):
+    try:
+        cursor.execute("DELETE FROM articles WHERE id = ?", (id))
+        conn.commit()
+    except Exception as e:
+        raise Exception("DELETE 중 오류 발생 ", e)
